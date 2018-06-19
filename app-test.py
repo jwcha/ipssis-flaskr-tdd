8caabe7bf55ee8bdb948a2b0e67d78b271806180
jwcha <neofanatic@gmail.com>
2018-06-19 06:10:28 -0700

    defined: functions to initialize, connect, close, and return the sqlite3 db; added: db initialization to main method.

--- app-test.py
+++ app-test.py
@@ -1,9 +1,12 @@
 from app import app
 
 import unittest
+import os
 
 
+# you can search through the file for TEST-###
 class BasicTestCase(unittest.TestCase):
+    # TEST-001
     # def test_index(self):
         # tester = app.test_client(self)
         # response = tester.get('/', content_type='html/text')
@@ -13,6 +16,10 @@ class BasicTestCase(unittest.TestCase):
         tester = app.test_client(self)
         response = tester.get('/', content_type='html/text')
         self.assertEqual(response.status_code, 404)
+
+    def test_database(self):
+        tester = os.path.exists("flaskr.db")
+        self.assertTrue(tester)
 
 
 if __name__ == '__main__':
